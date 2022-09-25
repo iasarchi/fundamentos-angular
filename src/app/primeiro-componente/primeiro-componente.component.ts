d1b496eccbe90fb5333541f2c00d82595026f7c0
IasArchi <iasmimcardoso11@gmail.com>
2022-09-25 14:15:55 -0300

    Primeira aula, criacao de componte

---
+++ src/app/primeiro-componente/primeiro-componente.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+import { reduce } from "rxjs";
+
+@Component({
+    selector:"app-primeiro-componente",
+    template:"<h2>Primeiro Compoonente </h2>",
+    styles: ["h2 { color: red; }"]
+})
+export class PrimeiroComponenteComponent{}
