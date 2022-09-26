93c9b8cab3d4cb265615f421b3bc0839ed15ceb6
IasArchi <iasmimcardoso11@gmail.com>
2022-09-26 19:02:57 -0300

    Estudo de Pipe Customizado

---
+++ src/app/segundo-componente/segundo-componente.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-segundo-componente',
+  templateUrl: './segundo-componente.component.html',
+  styleUrls: ['./segundo-componente.component.css']
+})
+export class SegundoComponenteComponent {
+  nome = "Joao";
+  dataNascimento = "1995-01-01"
+}
